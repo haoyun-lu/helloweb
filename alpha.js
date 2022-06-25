3b0541d21a005a6b75a48f5d8aa24e9ed38ef79a
Haoyun Lu <auskaoscar@outlook.com>
2022-06-26 00:08:38 +0800

    Initial commit

---
+++ alpha.js
@@ -0,0 +1,14 @@
+console.log('Hello World');
+let total = 1+3;
+if (1+1===2) {
+    console.log('MATH STILL WORKS DONT WORRY!');
+}
+
+
+//const fixArray = [1,2,3];
+// you can do these changes as long as orignal array is a reference
+//fixArray[0] = 100;
+//fixArray.push(900)
+// but you can not do this!!
+// constant won't allow to assgin new array to the orginal one!
+//fixArray = [3,4,5]
